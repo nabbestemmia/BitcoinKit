e1472d4817f5373e018d5880fd4d6047dab05b63
alessio <alessio@bemind.me>
2020-09-21 12:47:45 +0100

    removed iphoneis

--- setup/build_secp256k1.sh
+++ setup/build_secp256k1.sh
@@ -19,13 +19,13 @@ TARGETDIR_SIMULATOR="$CURRENTPATH/.build/iphonesimulator"
 mkdir -p "$TARGETDIR_SIMULATOR"
 
 (cd src && ./autogen.sh)
+(cd src && ./configure --host=x86_64-apple-darwin CC=`xcrun -find clang` CFLAGS="-O3 -arch i386 -arch x86_64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path` -fembed-bitcode-marker -mios-simulator-version-min=8.0" CXX=`xcrun -find clang++` CXXFLAGS="-O3 -arch i386 -arch x86_64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path` -fembed-bitcode-marker -mios-simulator-version-min=8.0" --prefix="$TARGETDIR_IPHONEOS" && make install)
 (cd src && ./configure --host=arm-apple-darwin CC=`xcrun -find clang` CFLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot `xcrun -sdk iphoneos --show-sdk-path` -fembed-bitcode -mios-version-min=8.0" CXX=`xcrun -find clang++` CXXFLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot `xcrun -sdk iphoneos --show-sdk-path` -fembed-bitcode -mios-version-min=8.0" --prefix="$TARGETDIR_SIMULATOR" && make install)
 
 cd -
 
 mkdir -p "$SCRIPT_DIR/../Libraries/secp256k1/lib"
-xcrun lipo -create "$TARGETDIR_IPHONEOS/lib/libsecp256k1.a" \
-                   "$TARGETDIR_SIMULATOR/lib/libsecp256k1.a" \
+xcrun lipo -create "$TARGETDIR_SIMULATOR/lib/libsecp256k1.a" \
                    -o "$SCRIPT_DIR/../Libraries/secp256k1/lib/libsecp256k1.a"
 cp -rf $TDIR/src/include "$SCRIPT_DIR/../Libraries/secp256k1"
 
